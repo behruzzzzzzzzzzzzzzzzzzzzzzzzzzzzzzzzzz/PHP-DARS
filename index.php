6a932acac4c29b5422a720560e9f8078893f8934
behruzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz <baxodirovbexruzbek33@gmail.com>
2025-11-13 16:21:27 +0500

    yangi

--- index.php
+++ index.php
@@ -1,5 +1,28 @@
-<?php
-echo "hello world";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$x = 10;
+$y = 5;
+
+$yigindi = $x+ $y;
+$ayirma = $x- $y;
+$kop = $x* $y;
+$bol = $x/ $y;
+
+
+  echo "yigindi $yigindi<br>";
+   echo "ayirma $ayirma<br>";
+    echo "kopaytma $kop<br>";
+     echo "bolinma $bol<br>";
+
+
+
 
 
 
@@ -9,3 +32,5 @@ echo "hello world";
 
 
 ?>
+</body>
+</html>
